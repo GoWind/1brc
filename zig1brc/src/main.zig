23fafbfebbb82bafcd9433e1230f89a6c6cdf52a
Govind <petrovgovind@gmail.com>
2024-01-09 16:10:54 +0100

    attempt to use handwritten parser for i32 instead of f32

--- zig1brc/src/main.zig
+++ zig1brc/src/main.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
 const File = std.fs.File;
 const numWorkers = 12;
-pub const Record = struct { min: f32 = 0, max: f32 = 0, total: f32 = 0, count: u32 = 0 };
+const SliceList = std.ArrayList([]const u8);
+const writer = std.io.getStdOut().writer();
+pub const Record = struct { min: i32 = 0, max: i32 = 0, total: i32 = 0, count: u32 = 0 };
 const TempMap = std.StringHashMap(Record);
 var threadMap: [numWorkers]TempMap = undefined;
 var threads: [numWorkers]std.Thread = undefined;
@@ -35,9 +37,9 @@ pub fn main() !void {
         threads[i].join();
     }
     const totalProcessed = try mergeMaps(threadAllocator, &globalMap, &threadMap);
-    printGlobalMap(globalMap);
+    try printGlobalMap(allocator, globalMap);
     const end = std.time.nanoTimestamp();
-    std.debug.print("{} rows took {d} nanoseconds\n", .{ totalProcessed, end - start });
+    try writer.print("{} rows took {d} nanoseconds\n", .{ totalProcessed, end - start });
 }
 
 fn calculate(
@@ -73,14 +75,14 @@ fn calculate(
             i = j + 1;
         } else if (file[j] == '\n') {
             num = file[i..j];
-            const temp = try std.fmt.parseFloat(f32, num);
+            const temp = parsei32(num);
 
             const maybeEntry = threadMap[idx].getEntry(city);
             if (maybeEntry) |entry| {
                 entry.value_ptr.*.count += 1;
                 entry.value_ptr.*.total += temp;
                 entry.value_ptr.*.max = @max(entry.value_ptr.*.max, temp);
-                entry.value_ptr.*.min = @max(entry.value_ptr.*.max, temp);
+                entry.value_ptr.*.min = @min(entry.value_ptr.*.min, temp);
             } else {
                 const rec = Record{ .count = 1, .min = temp, .max = temp, .total = temp };
                 const k = try allocator.alloc(u8, city.len);
@@ -120,10 +122,50 @@ fn mergeMaps(alloc: std.mem.Allocator, global: *TempMap, localMaps: []TempMap) !
     return totalCount;
 }
 
-fn printGlobalMap(map: TempMap) void {
-    var iterator = map.iterator();
-    while (iterator.next()) |kv| {
-        const record = kv.value_ptr.*;
-        std.debug.print("{s}: min: {d:3.1}, max:{d:3.1}, avg: {d:3.1}\n", .{ kv.key_ptr.*, record.min, record.max, @as(f32, record.total / @as(f32, @floatFromInt(record.count))) });
+fn printGlobalMap(allocator: std.mem.Allocator, map: TempMap) !void {
+    var keyList = SliceList.init(allocator);
+    defer keyList.deinit();
+    var keyIterator = map.keyIterator();
+    while (keyIterator.next()) |key| {
+        try keyList.append(key.*);
     }
+    std.mem.sort([]const u8, keyList.items, {}, (struct {
+        fn lessThan(_: void, a: []const u8, b: []const u8) bool {
+            return std.mem.lessThan(u8, a, b);
+        }
+    }).lessThan);
+
+    for (keyList.items) |key| {
+        const record = map.get(key).?;
+        const min = record.min;
+        const minf: f32 = @floatFromInt(min);
+        //const min: f32 = @as(f32, @floatFromInt(record.min) / 10.0);
+        const maxf: f32 = @floatFromInt(record.max);
+        const totalf: f32 = @floatFromInt(record.total);
+        const countf: f32 = @floatFromInt(record.count);
+        try writer.print("{s}: min: {d:3.1}, max:{d:3.1}, avg: {d:3.1}\n", .{ key, minf / 10.0, maxf / 10.0, totalf / (countf * 10.0) });
+    }
+}
+
+fn parsei32(s: []const u8) i32 {
+    var i: usize = 0;
+    var num: i32 = 0;
+    var mod: i32 = 1;
+    while (i < s.len) : (i += 1) {
+        if (s[i] == '-') {
+            mod = -1;
+        } else if (s[i] == '.') {} else {
+            num = num * 10 + @as(i32, s[i] - '0');
+        }
+    }
+    return num * mod;
+}
+
+test "test parsei32" {
+    try std.testing.expect(parsei32(&"123.4".*) == @as(i32, 1234));
+    try std.testing.expect(parsei32(&"23.4".*) == @as(i32, 234));
+    try std.testing.expect(parsei32(&"3.4".*) == @as(i32, 34));
+    try std.testing.expect(parsei32(&"3".*) == @as(i32, 3));
+    try std.testing.expect(parsei32(&"-123.4".*) == @as(i32, -1234));
+    try std.testing.expect(parsei32(&"-23.4".*) == @as(i32, -234));
 }
